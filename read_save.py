16ee9c9b0c1662370b9a828ec32283817c267d33
steve12512 <stevekalelis@hotmail.com>
2024-02-20 21:04:38 +0200

    hash dictionary created

--- read_save.py
+++ read_save.py
@@ -1,8 +1,10 @@
 import json
 
-#first we have to open our thesaurus file and iterate through it in order to create our nested dictionary. we have removed the intro and epilogue in order to make the reading easier
-#save our filepath
-def read_dictionary():
+
+def read_class_dictionary():
+    #first we have to open our thesaurus file and iterate through it in order to create our nested dictionary. we have removed the intro and epilogue in order to make the reading easier
+    #save our filepath
+    classes = {}
 
     # Initialize variables to track current class, division, section, and title
     current_class = None
@@ -19,61 +21,104 @@ def read_dictionary():
             #find classes
             if line.startswith('CLASS'):
                 current_class = line
-                dictionary[current_class] = {'sections': {}}
+                classes[current_class] = {'sections': {}}
                 current_division = None
                 current_section = None  # Reset current_section when encountering a new class
 
             #find divisions
             elif line.startswith('(Division'):
                 current_division = line
-                if current_class not in dictionary:
-                    dictionary[current_class] = {'sections': {}}
-                dictionary[current_class]['sections'][current_division] = {'words': []}
+                if current_class not in  classes:
+                    classes[current_class] = {'sections': {}}
+                classes[current_class]['sections'][current_division] = {'words': []}
                 current_section = None  # Reset current_section when encountering a new division
 
             #find sections
             elif line.startswith('SECTION'):
                 current_section = line
                 if current_division is not None:
-                    dictionary[current_class]['sections'][current_division]['sections'][current_section] = {'words': []}
+                    classes[current_class]['sections'][current_division]['sections'][current_section] = {'words': []}
                 else:
-                    dictionary[current_class]['sections'][current_section] = {'words': []}
+                    classes[current_class]['sections'][current_section] = {'words': []}
 
             #else the line has words, that have to be appended
             elif current_class is not None and current_section is not None:
                 
                 #to the division if there is one
                 if current_division is not None:
-                    dictionary[current_class]['sections'][current_division]['sections'][current_section]['words'].append(line)
-                    count += 1
+                    classes[current_class]['sections'][current_division]['sections'][current_section]['words'].append(line)
                 #else, they are appended to the section
                 else:
-                    dictionary[current_class]['sections'][current_section]['words'].append(line)
-                    count +=1
-        print(count)
-        
+                    classes[current_class]['sections'][current_section]['words'].append(line)
+    return classes 
 
 
 
+def read_hash():
+    #this function reads the dictionary to map # words to other words
 
-def save_dictionary():
-    #save out dictionary in json format
-    json_file_path = 'thesaurus.json'
+    #create another dict for  hash words
+    hash_dict = {}
+
+    # Open the file and iterate through it
+    with open('thesaurus.txt', 'r') as file:
+        current_number = None
+        current_word_list = None
+
+        for line in file:
+            # Remove leading and trailing whitespaces
+            line = line.strip()
+
+            # Check if the line starts with '#' followed by a number
+            if line.startswith('#'):
+                # Extract the number and create a new list for words
+                current_number = line.split('.', 1)[0]
+                current_word_list = [line.split('.', 1)[1].strip()]
+                hash_dict[current_number] = current_word_list
+            elif current_number is not None and not line.isupper():
+                # Append words to the current list, ignoring uppercase lines
+                current_word_list.append(line)
+
+    # Print the resulting dictionary
+    for key, value in hash_dict.items():
+        print(hash_dict.keys)
+
+    return hash_dict
+
+def save_hash_dictionary(hash_dict):
+    #save our hash dictionary in json format
+
+    json_file_path = 'hash.json'
+
+    with open(json_file_path, 'w') as json_file:
+        json.dump(hash_dict, json_file, indent= 2)
+
+    print('json file saved correctly 2')
+
+
+def save_class_dictionary(classes):
+
+    #save out class_dictionary in json format
+    json_file_path = 'classes.json'
 
 
     with open(json_file_path, 'w') as json_file:
-     json.dump(dictionary, json_file, indent=2)
+     json.dump(classes, json_file, indent=2)
 
     print('json file saved correctly')
 
+#START
+#read our file and store it in a dictionary based on classes, divisions, and sections
+classes = read_class_dictionary()
+
+#read our file and store it in another dictionary, based on hash numbers before word categories
+hash_dict = read_hash()
 
 
 
-#create an empty dict
-dictionary = {}
+#and save them both in json format
+save_class_dictionary(classes)
+save_hash_dictionary(hash_dict)
 
-#read our file
-read_dictionary()
 
-#and save it in json format
-save_dictionary()
+print(hash_dict['#1'])
