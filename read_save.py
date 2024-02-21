05320e8503665909ebf7d848b9802acd28e646d7
steve12512 <86954812+steve12512@users.noreply.github.com>
2024-02-21 15:42:07 +0200

    advancing with embeddings

--- read_save.py
+++ read_save.py
@@ -1,4 +1,8 @@
 import json
+from gensim.models import KeyedVectors
+import gensim.downloader as api
+import re
+from unidecode import unidecode
 
 
 def read_class_dictionary():
@@ -103,6 +107,46 @@ def save_class_dictionary(classes):
 
     print('json file saved correctly')
 
+
+def read_embeddings(hashdict, glove_vectors):
+    # Create an empty embeddings dict
+    embeddings_dict = {}
+    count1 = 0
+    count2 = 0
+
+    # Iterate through the list of words for each key in the hash dictionary
+    for key, word_list in hashdict.items():
+        # Assuming the first item in the list is the word
+        word = preprocess_word(word_list[0])
+        
+        try:
+            embeddings = glove_vectors[word]
+            embeddings_dict[key] = embeddings.tolist()
+            count1 += 1
+        except KeyError:
+            count2 += 1
+            
+    print(count1, count2)
+    return embeddings_dict
+
+
+
+def intialize_word2vec():
+    #initialize our models
+
+    glove_vectors = api.load('glove-twitter-25')
+    return glove_vectors
+
+
+def preprocess_word(word):
+    # Split the string using '.' as a delimiter and take the first part
+    word = word.split('.')[0]
+    # Remove diacritics and convert to lowercase
+    word = unidecode(word)
+    # Remove special characters and convert to lowercase
+    return re.sub(r'[^a-zA-Z0-9]', '', word).lower()
+
+
 #START
 #read our file and store it in a dictionary based on classes, divisions, and sections
 classes = read_class_dictionary()
@@ -115,5 +159,13 @@ hash_dict = read_hash()
 save_class_dictionary(classes)
 save_hash_dictionary(hash_dict)
 
+#now we have to initialize our word2vec
+glove_vectors = intialize_word2vec()
 
-print(hash_dict['#1'])
+#then we read our embeddings.
+embeddings = read_embeddings(hash_dict, glove_vectors)
+
+print(repr(hash_dict['#665'][0]))
+#print(embeddings['#665'])
+#print(glove_vectors.most_similar(hash_dict['#665'][0]))
+#print(embeddings['#1'])
